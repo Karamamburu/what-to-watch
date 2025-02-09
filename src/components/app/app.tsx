b0f32b1d89f0fa645891c26c7cbfc714b4603a60
Денис Хмыров <denis@Deniss-MacBook-Pro.local>
2025-02-09 23:35:35 +0300

    feat: private route added

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -6,18 +6,27 @@ import MyList from '../../pages/MyList/MyList';
 import MoviePage from '../../pages/MoviePage/MoviePage';
 import Player from '../../pages/Player/Player';
 import NotFound from '../../pages/NotFound/NotFound';
+import { AuthorizationStatus } from '../../constants/const';
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/mylist" element={<MyList />} />
-        <Route path="/films/:id" element={<MoviePage />} />
-        <Route path="/films/:id/review" element={<AddReview />} />
-        <Route path="/player/:id" element={<Player />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path='/' element={<MainPage />} />
+        <Route path='/login' element={<SignIn />} />
+        <Route
+          path='/mylist'
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <MyList />
+            </PrivateRoute>
+          }
+        />
+        <Route path='/films/:id' element={<MoviePage />} />
+        <Route path='/films/:id/review' element={<AddReview />} />
+        <Route path='/player/:id' element={<Player />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
