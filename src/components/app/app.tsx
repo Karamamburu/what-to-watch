e496353a86dc6f741d39ab0eb8f737e717dfd939
Денис Хмыров <denis@Deniss-MacBook-Pro.local>
2025-02-09 23:00:21 +0300

    feat: all components imported into app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,19 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import AddReview from '../../pages/AddReview/AddReview';
 import MainPage from '../../pages/MainPage/MainPage';
+import HeadGuest from '../../pages/HeadGuest/HeadGuest';
+import MoviePage from '../../pages/MoviePage/MoviePage';
+import MoviePageDetails from '../../pages/MoviePageDetails/MoviePageDetails';
+import MoviePageInList from '../../pages/MoviePageInList/MoviePageInList';
+import MoviePageReviews from '../../pages/MoviePageReviews/MoviePageReviews';
+import MyList from '../../pages/MyList/MyList';
+import NotFound from '../../pages/NotFound/NotFound';
+import Player from '../../pages/Player/Player';
+import PlayerPause from '../../pages/PlayerPause/PlayerPause';
+import SignIn from '../../pages/SignIn/SignIn';
+import SignInError from '../../pages/SignInError/SignInError';
+import SignInMessage from '../../pages/SignInMessage/SignInMessage';
+
 function App(): JSX.Element {
   return (
     <MainPage />
